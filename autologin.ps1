a639836059d647be13ce7e45bc1cce3280dd9df6
JeanLm88 <86303827+JeanLm88@users.noreply.github.com>
2022-10-11 15:01:23 -0300

    Add files via upload

---
+++ autologin.ps1
@@ -0,0 +1,19 @@
+﻿
+$ESCOLHA = Read-Host ("""SEJA BEM VINDO(A), O QUE DESEJA FAZER?
+[0]DELETAR USER  [1] ADICIONAR LOGIN AUTOMÁTICO""")
+[int] $DEL = 0
+[int]$ADC = 1
+if ([int]$ESCOLHA -match [int]$DEL){
+    $RegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+    Set-ItemProperty $RegistryPath 'DefaultUserName' -Value "" -type String 
+    Set-ItemProperty $RegistryPath 'DefaultPassWord' -Value "" -type String
+    Write-Warning "USER DELETADO"
+}else{
+$Username = Read-Host 'Usuário'
+$Pass = Read-Host "Senha do $Username "
+$RegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+Set-ItemProperty $RegistryPath 'AutoAdminLogon' -Value "1" -Type String 
+Set-ItemProperty $RegistryPath 'DefaultUserName' -Value "atmosfera.net\$Username" -type String 
+Set-ItemProperty $RegistryPath 'DefaultPassWord' -Value "$Pass" -type String
+Write-Warning "Auto-Login do $username configurado. Por favor reinicie o computador"
+}
